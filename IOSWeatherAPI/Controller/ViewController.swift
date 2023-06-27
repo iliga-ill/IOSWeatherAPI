b4502a73c2e49af2262c94b4779f033cc1c9be33
iliga-ill <illvolkov@mail.ru>
2023-06-27 22:14:47 +0300

    init

---
+++ IOSWeatherAPI/Controller/ViewController.swift
@@ -0,0 +1,64 @@
+
+import UIKit
+import CoreLocation
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    var networkWeatherManager = NetworkWeatherManager()
+    lazy var locationManager: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        self.presentSearchAlertConroller(withTitle: "Enter city name", message: nil, style: .alert) { city in
+            self.networkWeatherManager.fetchCurrentWeather(forRequestType: .cityName(city: city))
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        networkWeatherManager.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
+    }
+}
+
+extension ViewController: NetworkWeatherManagerDelegate {
+    func updateInterface(_: NetworkWeatherManager, with weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString 
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+            
+        }
+    }
+}
+ 
+//MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        let latitude = location.coordinate.latitude
+        let longtitude = location.coordinate.longitude
+        networkWeatherManager.fetchCurrentWeather(forRequestType: .coordinate(latitude: latitude, longtitude: longtitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
---
+++ IOSWeatherAPI/Model/Constants.swift
@@ -0,0 +1,3 @@
+import Foundation
+
+let apiKey = "5b683b1b0acc488d1d164f35aa9227ce"
